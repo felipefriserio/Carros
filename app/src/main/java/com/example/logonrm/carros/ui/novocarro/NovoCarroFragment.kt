6cfdbec14e619dfd2260b0a7962b1c62576b9554
logonrm <logonrm@fiap.com.br>
2018-03-10 17:14:19 -0300

    lista carros buscados do banco na nuvem

--- app/src/main/java/com/example/logonrm/carros/ui/novocarro/NovoCarroFragment.kt
+++ app/src/main/java/com/example/logonrm/carros/ui/novocarro/NovoCarroFragment.kt
@@ -14,10 +14,7 @@ import com.example.logonrm.carros.R
  * A simple [Fragment] subclass.
  */
 class NovoCarroFragment : Fragment() {
-
-
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater!!.inflate(R.layout.fragment_novo_carro, container, false)
     }
